71f5f2ddda5449bb2e76c27fb221500b1f39df2a
Nirvana <stephenarthur2707@gmail.com>
2023-12-13 19:17:26 -0800

    added(new)

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,13 +1,13 @@
 import {createUserWithEmailAndPassword,onAuthStateChanged} from "firebase/auth"
 import {auth,db} from "/src/firebase"
-import { collection, setDoc, doc } from "firebase/firestore"
+import { collection, setDoc, doc ,getDoc,addDoc} from "firebase/firestore"
 import Swal from "sweetalert2"
 
 const registerForm = document.querySelector(".registerForm")
 const loader = document.querySelector(".loader-box")
-
-
 const error = document.querySelector(".error-display")
+
+
   async function registerUser(){
     if (registerForm.status.value !== "Admin" && registerForm.status.value !== "Cashier") {
         error.style.display = "block";
@@ -15,113 +15,147 @@ const error = document.querySelector(".error-display")
         try{
             loader.style.display = "block";
             await createUserWithEmailAndPassword(auth,registerForm.email.value,registerForm.password.value)
-            .then(()=>{
-             onAuthStateChanged(auth,(user)=>{
+            onAuthStateChanged(auth,(user)=>{
                 if(user){
-                        const adminCollection = (collection(db,"Admins"))
-                        const cashierCollection = (collection(db,"Cashiers"))
-                        const adminDoc = doc(adminCollection,user.uid)
-                        const cashierDoc = doc(cashierCollection,user.uid)
+                    const uid = user.uid;
+                    const adminCollection = (collection(db,"Admins"))
+                    const cashierCollection = (collection(db,"Cashiers"))
+                    const adminDoc = doc(adminCollection,uid)
+                    const cashierDoc = doc(cashierCollection,uid)
 
-                        if(registerForm.status.value === "Admin"){
-                            setDoc(adminDoc,{
-                                Role: registerForm.status.value,
-                                Username: registerForm.username.value.trim(),
-                                cart:[],
-                                order:[]
-                            })
-                            console.log("Admin created succesfully")
-                            registerForm.reset()
-                            Swal.fire({
-                                position: "center",
-                                icon: "success",
-                                title: "Admin created successfully",
-                                showConfirmButton: false,
-                                timer: 1500
-                              });
-                              setTimeout(()=>{
-                                window.location.href = "/dist/index.html"
-                              },1500)
-                       
-                        }
-                        if(registerForm.status.value === "Cashier"){
-                            setDoc(cashierDoc,{
-                                Role:registerForm.status.value,
-                                Username: registerForm.username.value.trim(),
-                                cart:[],
-                                order:[]
-                            })
-                            console.log("Cashier created succesfully")
-                            registerForm.reset()
-                            Swal.fire({
-                                position: "center",
-                                icon: "success",
-                                title: "Cashier created successfully",
-                                showConfirmButton: false,
-                                timer: 1500
-                              });
-                              setTimeout(()=>{
-                                window.location.href = "/dist/index.html"
-                              },1500)
-                        }
+                    if(registerForm.status.value === "Admin"){
+                        setDoc(adminDoc,{
+                           Role: registerForm.status.value,
+                           Username: registerForm.username.value.trim(),
+                           cart:[],
+                           order:[]
+                       })
+                       console.log("Admin created succesfully")
+                       registerForm.reset()
+                       Swal.fire({
+                           position: "center",
+                           icon: "success",
+                           title: "Admin created successfully",
+                           showConfirmButton: false,
+                           timer: 1500
+                         });
+                         setTimeout(()=>{
+                           window.location.href = "/dist/index.html"
+                         },1500)
+                  
+                   }
+                   if(registerForm.status.value === "Cashier"){
+                       setDoc(cashierDoc,{
+                           Role:registerForm.status.value,
+                           Username: registerForm.username.value.trim(),
+                           cart:[],
+                           order:[]
+                       })
+                       console.log("Cashier created succesfully")
+                       registerForm.reset()
+                       Swal.fire({
+                           position: "center",
+                           icon: "success",
+                           title: "Cashier created successfully",
+                           showConfirmButton: false,
+                           timer: 1500
+                         });
+                         setTimeout(()=>{
+                           window.location.href = "/dist/index.html"
+                         },1500)
+                   }
                 }
-                   
-             })
-              
-        
             })
-           .catch((error)=>{
-            console.log(error)
-            loader.style.display = "none"
-            if(error.code === "auth/invalid-email"){
-                Swal.fire({
-                    icon: "error",
-                    title: "Email is Incorrect",
-                  });
-            }else if (error.code === 'auth/wrong-password') {
-                Swal.fire({
-            icon: "error",
-            title: "Please check your password",
-            });
-            } else if (error.code === 'auth/user-not-found') {
-                Swal.fire({
-            icon: "error",
-            title: "No User Was Found",
-            });
-            } else if (error.code === 'auth/email-already-in-use') {
-                Swal.fire({
-            icon: "error",
-            title: "Email Already Exists",
-            });
-            }
-            else if (error.code === 'auth/weak-password') {
-                Swal.fire({
-            icon: "error",
-            title: "Password should be 6 characters or more",
-            });
-            }
-            else {
-              (error.code==='auth/network-request-failed')
-              Swal.fire({
-                icon: "error",
-                title: "Check Your Internet Connection",
-                });
-            }
-            
-           })
-        }
-        catch(error){
+
+                      
+           
+        }catch(error){
            console.log(error)
+           loader.style.display = "none"
+           if(error.code === "auth/invalid-email"){
+               Swal.fire({
+                   icon: "error",
+                   title: "Email is Incorrect",
+                 });
+           }else if (error.code === 'auth/wrong-password') {
+               Swal.fire({
+           icon: "error",
+           title: "Please check your password",
+           });
+           } else if (error.code === 'auth/user-not-found') {
+               Swal.fire({
+           icon: "error",
+           title: "No User Was Found",
+           });
+           } else if (error.code === 'auth/email-already-in-use') {
+               Swal.fire({
+           icon: "error",
+           title: "Email Already Exists",
+           });
+           }
+           else if (error.code === 'auth/weak-password') {
+               Swal.fire({
+           icon: "error",
+           title: "Password should be 6 characters or more",
+           });
+           }
+           else {
+             (error.code==='auth/network-request-failed')
+             Swal.fire({
+               icon: "error",
+               title: "Check Your Internet Connection",
+               });
+           }
         }
     }
          
  }
+
+ async function createCustomerCollection(uid){
+    const adminCollection = doc(db,"Admins",uid);
+    const cashierCollection = doc(db,"Cashiers",uid);
+    const adminDoc = await getDoc(adminCollection);
+    const cashierDoc = await getDoc(cashierCollection);
+    try{
+        if(adminDoc.exists()){
+            const customerCollection = collection(adminCollection,"Customers");
+            addDoc(customerCollection,{
+                CustomerName:"",
+                CustomerPhone:""
+            })
+            console.log("inside admin")
+            console.log("customer created successfully")
+        }else if(cashierDoc.exists()){
+            const customerCollection = collection(cashierCollection,"Customers");
+            addDoc(customerCollection,{
+                CustomerName:"",
+                CustomerPhone:""
+            })
+            console.log("inside cashier")
+            console.log("customer created successfully")
+        
+    }
+}
+    catch(error){
+       console.log(error)
+    }
+
+  }
+
         
    
 
 registerForm.addEventListener("submit",(event)=>{
     event.preventDefault()
     registerUser()
+
+    onAuthStateChanged(auth,(user)=>{
+        if(user){
+            const uid = user.uid;
+            createCustomerCollection(uid)
+        }
+    })
+  
 })
 
 
