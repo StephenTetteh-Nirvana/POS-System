c50f112547a4076ecafe5a2e0081f4221243146d
Nirvana <stephenarthur2707@gmail.com>
2023-12-16 17:11:12 -0800

    added(new)

--- src/pages/Orders/index.js
+++ src/pages/Orders/index.js
@@ -10,6 +10,7 @@ const PopUp = document.querySelector(".popup-order-summary")
 const PopUpOrders = document.querySelector(".order-summary-order")
 const loader = document.querySelector(".loader-box")
 const bottomInfo = document.querySelector(".bottom-section")
+const customerLoader = document.querySelector(".customer-loader-box")
 
 
 document.addEventListener("DOMContentLoaded",function(){
@@ -37,6 +38,7 @@ document.addEventListener("DOMContentLoaded",function(){
 })
 
 async function displayCustomers(uid){
+    customerLoader.style.display = " block"
    const adminCollection = doc(db,"Admins",uid)
    const cashierCollection = doc(db,"Cashiers",uid)
    const adminDoc = await getDoc(adminCollection)
@@ -56,10 +58,11 @@ async function displayCustomers(uid){
         Info.innerHTML += `<ul class="customer-info">
                             <li>${customerInfo.name}</li>
                             <li>${customerInfo.phone}</li>
-                            <li>${adminDoc.data().Username}<span class="status">(${adminDoc.data().Role})</span></li>
+                            <li>${adminDoc.data().Username}<span class="status">${adminDoc.data().Role}</span></li>
                             <button class="btn" data-id="${customer.id}">View</button>
                           </ul>
                           `
+        customerLoader.style.display = "none"
     })
    
    }else if(cashierDoc.exists()){
@@ -72,12 +75,13 @@ async function displayCustomers(uid){
         }
         console.log(customerInfo)
         Info.innerHTML += `<ul class="customer-info">
-                            <li>${customerInfo.name}</li>
+                           <li>${customerInfo.name}</li>
                             <li>${customerInfo.phone}</li>
-                            <li>${cashierDoc.data().Username}<span class="status">(${cashierDoc.data().Role})</span></li>
-                            <button class="btn">View</button>
-                          </ul>
-                          `
+                            <li>${cashierDoc.data().Username}<span class="cashier-status">${cashierDoc.data().Role}</span></li>
+                            <button class="btn"  data-id="${customer.id}" >View</button>
+                          </ul>`
+        customerLoader.style.display = "none"
+
     })
    }
 
@@ -153,14 +157,51 @@ async function displayOrders(uid,documentId){
            }
         })
        
-
-
-        
     }else if(cashierDoc.exists()){
-        const customerCollection = collection(adminCollection,"Customers");
-        const snapshot = await getDocs(customerCollection)
-        console.log(snapshot)
-    }
+        const CashiercustomerCollection = collection(cashierCollection,"Customers");
+        const snapshot = await getDocs(CashiercustomerCollection)
+        snapshot.forEach((customer) => {
+           if(customer.id === documentId){
+            console.log(documentId)
+            console.log("id match")
+            const orderData = customer.data().order;
+            console.log(orderData)
+            orderData.forEach((order) => {
+                const orderItem = {
+                    name:order.Name,
+                    price:order.Price,
+                    image:order.Image,
+                    quantity:order.quantity
+                }
+                console.log(orderItem.image)
+                PopUpOrders.innerHTML += `<div class="order">
+
+                                            <div class="order-image-box">
+                                            <img src="${orderItem.image}"/>
+                                            </div>
+                                                <div>
+                                                <h3>${orderItem.name}</h3>
+                                                </div>
+
+                                                    <div>
+                                                    <h3>${orderItem.quantity} piece(s)</h3>
+                                                    </div>
+
+                                                        <div>
+                                                        <h3>${orderItem.price}.00</h3>
+                                                        </div>
+
+                                            </div>`
+                    loader.style.display = "none"
+                    bottomInfo.style.display = "block"
+                
+            })
+           }else{
+            console.log("no match:",customer.id)
+           }
+        })
+       
+   }
 }
 
 
